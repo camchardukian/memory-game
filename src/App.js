cb32d85f7864117e1a70663254ee8a8e987e7318
Cameron Chardukian <camchardukian@gmail.com>
2021-08-25 19:29:56 +0700

    feature/flip-card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import CardMatchingPage from "./containers/CardMatchingPage";
 import { TWELVE_CARDS_DEFAULT_DATA } from "./utils/constants";
-function App() {
+const App = () => {
+  const [cardsArray, setCardsArray] = useState(TWELVE_CARDS_DEFAULT_DATA);
+  const handleClickCard = (index) => {
+    let updatedCardsArray = [...cardsArray];
+  updatedCardsArray[index]['isFlippedToBack'] = !updatedCardsArray[index]['isFlippedToBack']
+    setCardsArray(updatedCardsArray);
+  };
   return (
     <div className="App">
-      <CardMatchingPage cardsArray={TWELVE_CARDS_DEFAULT_DATA} />
+      <CardMatchingPage
+        cardsArray={TWELVE_CARDS_DEFAULT_DATA}
+        onClickCard={handleClickCard}
+      />
     </div>
   );
-}
+};
 
 export default App;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,16 +4,16 @@ export const CARD_TYPES = {
 };
 
 export const TWELVE_CARDS_DEFAULT_DATA = [
-  { type: "text", content: "1" },
-  { type: "text", content: "1" },
-  { type: "text", content: "2" },
-  { type: "text", content: "2" },
-  { type: "text", content: "3" },
-  { type: "text", content: "3" },
-  { type: "text", content: "4" },
-  { type: "text", content: "4" },
-  { type: "text", content: "5" },
-  { type: "text", content: "5" },
-  { type: "text", content: "6" },
-  { type: "text", content: "6" },
+  { type: "text", content: "1", isFlippedToBack: true },
+  { type: "text", content: "1", isFlippedToBack: true },
+  { type: "text", content: "2", isFlippedToBack: true },
+  { type: "text", content: "2", isFlippedToBack: true },
+  { type: "text", content: "3", isFlippedToBack: true },
+  { type: "text", content: "3", isFlippedToBack: true },
+  { type: "text", content: "4", isFlippedToBack: true },
+  { type: "text", content: "4", isFlippedToBack: true },
+  { type: "text", content: "5", isFlippedToBack: true },
+  { type: "text", content: "5", isFlippedToBack: true },
+  { type: "text", content: "6", isFlippedToBack: true },
+  { type: "text", content: "6", isFlippedToBack: true },
 ];
