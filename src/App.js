25143d0abdfbe157ea2e0cd432f72e00762b6b1a
cameron <c2c.cameron.ch@gmail.com>
2021-08-28 19:58:55 +0700

    feature/limit-flipped-cards -- set a limit of two cards flipped to the front side at any given time

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React, { useState } from "react";
-import CardMatchingPage from "./containers/CardMatchingPage";
-import { TWELVE_CARDS_DEFAULT_DATA } from "./utils/constants";
-const App = () => {
-  const [cardsArray, setCardsArray] = useState(TWELVE_CARDS_DEFAULT_DATA);
-  const handleClickCard = (index) => {
-    let updatedCardsArray = [...cardsArray];
-  updatedCardsArray[index]['isFlippedToBack'] = !updatedCardsArray[index]['isFlippedToBack']
-    setCardsArray(updatedCardsArray);
-  };
-  return (
-    <div className="App">
-      <CardMatchingPage
-        cardsArray={TWELVE_CARDS_DEFAULT_DATA}
-        onClickCard={handleClickCard}
-      />
-    </div>
-  );
-};
+import React, { useState, useEffect } from 'react'
+import CardMatchingPage from './containers/CardMatchingPage'
+import { TWELVE_CARDS_DEFAULT_DATA } from './utils/constants'
 
-export default App;
+const App = () => {
+   const [cardsArray, setCardsArray] = useState(TWELVE_CARDS_DEFAULT_DATA)
+   const [numberOfFlippedCards, setNumberOfFlippedCards] = useState(0)
+
+   useEffect(() => {
+      setNumberOfFlippedCards(() => {
+         return cardsArray.reduce((a, c) => (c.isFlippedToBack === false ? ++a : a), 0)
+      })
+   }, [cardsArray])
+
+   const handleClickCard = (index) => {
+      console.log('cardsArray[index]', cardsArray[index]['isFlippedToBack'])
+      if (numberOfFlippedCards < 2 || !cardsArray[index]['isFlippedToBack']) {
+         let updatedCardsArray = [...cardsArray]
+         updatedCardsArray[index]['isFlippedToBack'] =
+            !updatedCardsArray[index]['isFlippedToBack']
+         setCardsArray(updatedCardsArray)
+      }
+   }
+   return (
+      <div className="App">
+         <CardMatchingPage
+            cardsArray={TWELVE_CARDS_DEFAULT_DATA}
+            onClickCard={handleClickCard}
+         />
+      </div>
+   )
+}
+
+export default App
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { CARD_TYPES } from "../../utils/constants";
-import "./styles.scss";
+import React from 'react'
+import { CARD_TYPES } from '../../utils/constants'
+import './styles.scss'
 const Card = ({ cardInfo, onClick, index }) => {
-  const { type, content, isFlippedToBack } = cardInfo;
-  if (type === CARD_TYPES.IMAGE) {
-    return <div>images in development...</div>;
-  } else if (type === CARD_TYPES.TEXT)
-    return (
-      <div className="card" onClick={() => onClick(index)}>
-        <div className="card-inner-content-container">
-          <div
-            className={`card-inner-content ${
-              isFlippedToBack ? "flipped-inner-content" : ""
-            }`}
-          >
-            <div className="card-front">{content}</div>
-            <div className="card-back">?</div>
-          </div>
-        </div>
-      </div>
-    );
-};
+   const { type, content, isFlippedToBack } = cardInfo
+   if (type === CARD_TYPES.IMAGE) {
+      return <div>images in development...</div>
+   } else if (type === CARD_TYPES.TEXT)
+      return (
+         <div className="card" onClick={() => onClick(index)}>
+            <div className="card-inner-content-container">
+               <div
+                  className={`card-inner-content ${
+                     isFlippedToBack ? 'flipped-inner-content' : ''
+                  }`}
+               >
+                  <div className="card-front">{content}</div>
+                  <div className="card-back">?</div>
+               </div>
+            </div>
+         </div>
+      )
+}
 
-export default Card;
+export default Card
