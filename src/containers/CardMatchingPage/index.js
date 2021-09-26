baa15d99770f05d2f93fb67324b7eea6d97036ce
cameron <c2c.cameron.ch@gmail.com>
2021-09-26 18:49:50 +0700

    feature/post-game -- After the user matches all of the cards they receive a message that they've won and are given the option to return to the home screen or play again using the same cards.

--- src/containers/CardMatchingPage/index.js
+++ src/containers/CardMatchingPage/index.js
@@ -4,18 +4,22 @@ import Card from '../../components/Card'
 const CardMatchingPage = ({ cardsArray = [], onClickCard }) => {
    return (
       <div className="card-matching-page-container">
-         <div className="cards-list">
-            {cardsArray.map((cardInfo, index) => {
-               return (
-                  <Card
-                     key={index}
-                     index={index}
-                     onClick={(index) => onClickCard(index)}
-                     cardInfo={cardInfo}
-                  />
-               )
-            })}
-         </div>
+         {cardsArray.length > 0 ? (
+            <div className="cards-list">
+               {cardsArray.map((cardInfo, index) => {
+                  return (
+                     <Card
+                        key={index}
+                        index={index}
+                        onClick={(index) => onClickCard(index)}
+                        cardInfo={cardInfo}
+                     />
+                  )
+               })}
+            </div>
+         ) : (
+            <div className="winner-text">YOU WIN!!!!</div>
+         )}
       </div>
    )
 }
